65d313a00fb87d74c173dbec6830e76cb51e9822
Hiroshi Sakai <ziguzagu@gmail.com>
2008-05-14 07:32:57 +0000

    Added Bundle::TypeCast to install cpan modules that TypeCast depended.
    git-svn-id: https://typecastmobile.googlecode.com/svn/trunk@5 bb8930e5-1c4d-0410-9fc4-07b5614f54de

---
+++ lib/Bundle/TypeCast.pm
@@ -0,0 +1,37 @@
+# $Id$
+package Bundle::TypeCast;
+
+our $VERSION = '0.1';
+
+1;
+__END__
+
+=head1 NAME
+
+Bundle::LWP - install all libwww-perl related modules
+
+=head1 SYNOPSIS
+
+ perl -MCPAN -e 'install Bundle::TypeCast'
+
+=head1 CONTENTS
+
+HTTP::MobileAgent
+
+UNIVERSAL::isa
+
+Error
+
+Error::Simple
+
+CSS
+
+CSS::Parse::Packed
+
+Class::Singleton
+
+Date::Parse
+
+HTML::Split
+
+=cut
